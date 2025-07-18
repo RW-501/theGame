15ddb53fc39370d9a20392dd5449b49c59711aaa
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-07-18 07:35:57 -0500

    up

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,5 @@
-exports.updateStockPrices = functions.pubsub.schedule("every 2 minutes").onRun(async () => {
-  const snapshot = await db.collection("stocks").get();
+const functions = require("firebase-functions");
 
-  const updates = snapshot.docs.map(async (doc) => {
-    const symbol = doc.id;
-    const newPrice = generateRandomPrice(100, 500);
-
-    await db.collection("stocks").doc(symbol).set({
-      price: newPrice,
-      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-    }, { merge: true });
-
-    console.log(`Updated ${symbol} to $${newPrice}`);
-  });
-
-  await Promise.all(updates);
-  return null;
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase!");
 });
